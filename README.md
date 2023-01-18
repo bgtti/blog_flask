53acd1d42505206883c26599fbf98137976283c7
bgtti <bru.mgtti@gmail.com>
2023-01-18 22:34:33 +0100

    db creation and email setup

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ A responsive travel blog template created using python flask and bootstrap.
 ![Preview of app](Blog_Preview_Posts.png)
 
 ![Preview of app](Blog_Preview_Responsive.png)
+
+## Receiving messages
+Messages sent though the contact form are sent and received by a gmail account.
+The credentials (email address and password) have been saved to an .env file, and the variables imported into main.py. This information must be replaced for the code to work on local by anyone willing to use the codebase.
+
+## Database
+Messages received through the contact form are stored in an SQLite database.
+
--- main.py
+++ main.py
@@ -1,11 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from posts import the_posts
 from themes import the_themes
 from authors import the_authors
 import copy
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime 
+import smtplib
+import os #getting .env
+from dotenv import load_dotenv  # .env
 
 app = Flask(__name__)
 
+# EMAIL
+load_dotenv() # used to get .env variables, where username and password for the email account are stored
+EMAIL = os.getenv('EMAIL_ADDRESS')  # put your email here, used for the sender and receiver
+PASSWORD = os.getenv('EMAIL_PASSWORD')  # put your password here
+
+def send_email(form_user_name, form_user_email, form_user_message):
+    SUBJECT = f"Subject: Travel Blog New Message from {form_user_name}"
+    MESSAGE = f"""\
+Contact name: {form_user_name}
+Contact email: {form_user_email}
+Message:
+{form_user_message}
+    """
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com") as connection:
+            connection.login(EMAIL, PASSWORD)
+            connection.sendmail(from_addr=EMAIL, to_addrs=EMAIL,
+                                msg=f"{SUBJECT}\n\n{MESSAGE}")
+    except:
+        return "There was an error sending your message."
+
+
+# DATABASE
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///contact.db'
+db = SQLAlchemy(app) #initializes database
+
+# Create DB module for contact
+class Blog_Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    email = db.Column(db.String(100), nullable=False)
+    message = db.Column(db.String(700))
+
+    # Create function to create a string
+    def __repr__(self):
+        return '<Name_Message %r>' % self.id
+
+with app.app_context():
+    db.create_all()
+
+# BLOG
 @app.route("/")
 def home():
     posts_all = copy.deepcopy(the_posts)
@@ -52,6 +99,30 @@ def about():
         })
     return render_template('about.html', authors_all=authors_all)
 
+
+@app.route("/contact/", methods=['POST', 'GET'])
+def contact():
+    if request.method=="POST":
+        contact_name = request.form['contact_name']
+        contact_email = request.form['contact_email']
+        contact_message = request.form['contact_message']
+        new_contact = Blog_Contact(
+            name=contact_name, email=contact_email, message=contact_message)
+        print(contact_name)
+        print(contact_email)
+        print(contact_message)
+        try:
+            # push to database:
+            db.session.add(new_contact)
+            db.session.commit()
+            # send email:
+            send_email(contact_name, contact_email, contact_message)
+            return render_template('contact.html', msg_sent=True)
+        except:
+            return "There was an error adding message to the database."
+    my_contacts = db.session.query(Blog_Contact).all()
+    print(my_contacts)
+    return render_template('contact.html', msg_sent=True)
 
 @app.route("/post/<int:index>")
 def blog_post(index):
