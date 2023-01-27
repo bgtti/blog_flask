54800e00e7dbb74483122b61f1dfab80be728ef5
bgtti <bru.mgtti@gmail.com>
2023-01-27 23:39:23 +0100

    rich text editor

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from contact import send_email
 from helpers import hash_pw
 from werkzeug.security import generate_password_hash, check_password_hash  # used in login
-from flask_ckeditor import CKEditor #add new blog posts
-
+from flask_ckeditor import CKEditor, CKEditorField #add new blog posts
 
 
 app = Flask(__name__)
@@ -23,8 +22,9 @@ app.config['SECRET_KEY'] = "myFlaskApp4Fun"  # needed for login with wtforms
 
 # DATABASE
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///admin.db'
-app.config['SQLALCHEMY_BINDS'] = {
-    'author': 'sqlite:///author.db', 'user': 'sqlite:///user.db', 'theme': 'sqlite:///theme.db', 'posts': 'sqlite:///posts.db', 'contact': 'sqlite:///contact.db'}
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_BINDS'] = {
+#     'blog_author': 'sqlite:///blog_author.db', 'blog_user': 'sqlite:///blog_user.db', 'blog_theme': 'sqlite:///blog_theme.db', 'blog_posts': 'sqlite:///blog_posts.db', 'blog_contact': 'sqlite:///blog_contact.db'}
 db = SQLAlchemy(app) #initializes database
 
 login_manager = LoginManager()
@@ -35,7 +35,8 @@ def load_user(user_id):
     return Blog_User.query.get(int(user_id))
 
 class Blog_User(UserMixin, db.Model):
-    __bind_key__ = 'user'
+    __tablename__ = "blog_user"
+    # __bind_key__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False, unique=True)
     email = db.Column(db.String(200), nullable=False, unique=True)
@@ -50,15 +51,18 @@ class Blog_User(UserMixin, db.Model):
     # whether user has been blocked (future implementation)
     blocked = db.Column(db.String(5), default="FALSE")
     likes = db.Column(db.Integer, default=0)
+    # posts = db.Column(db.Integer, db.ForeignKey('blog_posts.id'))
+    posts = db.relationship('Blog_Posts', backref='author')
 
     def __repr__(self):
         return f"<User: {self.id} {self.name} {self.email}>"
 
 # Blog Posts
 class Blog_Posts(db.Model):
+    __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
     theme = db.Column(db.String(200), nullable=False)
-    author = db.Column(db.String(200), nullable=False)
+    # author = db.Column(db.String(200), nullable=False)
     date_submitted = db.Column(db.DateTime, default=datetime.utcnow)
     date_to_post = db.Column(db.DateTime, default=datetime.utcnow)
     title = db.Column(db.String(200), nullable=False)
@@ -77,13 +81,16 @@ class Blog_Posts(db.Model):
     featured = db.Column(db.String(5), default="FALSE")
     likes = db.Column(db.Integer, default=0)
     comments = db.Column(db.Integer, default=0)
+    # blog_user = db.relationship('Blog_User', backref='the_author')
+    author_id = db.Column(db.Integer, db.ForeignKey('blog_user.id'))
 
     def __repr__(self):
         return f"<Post: {self.id} {self.theme} {self.title}>"
 
 # Create DB module for contact (stores messages sent via contact form)
 class Blog_Contact(db.Model):
-    __bind_key__ = 'contact'
+    __tablename__ = "blog_contact"
+    # __bind_key__ = 'contact'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
@@ -94,16 +101,6 @@ class Blog_Contact(db.Model):
 #     def __repr__(self):
 #         return '<Name_Message %r>' % self.id
 
-
-
-# example to include foreign key:
-# employees = Table(
-#     "employees",
-#     metadata_obj,
-#     Column("employee_id", Integer, primary_key=True),
-#     Column("employee_name", String(60), nullable=False),
-#     Column("employee_dept", Integer, ForeignKey("departments.department_id")),
-# )
 
 with app.app_context():
     db.create_all()
@@ -117,18 +114,26 @@ with app.app_context():
         # db.session.commit()
         # creating some dummie accounts (for testing, delete the bellow later):
         RANDOM_PW = hash_pw("user123")
-        random1 = Blog_User(name="Maria", email="m@m", password=RANDOM_PW, type="user")
+        random1 = Blog_User(name="Maria", email="m@m", password=RANDOM_PW, type="author")
         random2 = Blog_User(name="John Meyers", email="j@m", password=RANDOM_PW, type="author")
         random3 = Blog_User(name="Fabienne123", email="f@f", password=RANDOM_PW, type="user")
         random4 = Blog_User(name="Kokaloka", email="k@k", password=RANDOM_PW, type="user")
         random5 = Blog_User(name="SublimePoster", email="s@p", password=RANDOM_PW, type="user")
+        # creating dummie posts (for testing, delete the bellow later):
+        post1 = Blog_Posts(theme="Beach", title="Cool post", intro="About this", body="This is the body of the post.", author_id=2)
+        post2 = Blog_Posts(theme="City", title="Cool post 2", intro="About this again", body="This is the body of the 2nd post.", author_id=3)
+        post3 = Blog_Posts(theme="City", title="Kinda cool post", intro="About that", body="This is the body of another post.", author_id=2)
         db.session.add(random1)
         db.session.add(random2)
         db.session.add(random3)
         db.session.add(random4)
         db.session.add(random5)
+        db.session.add(post1)
+        db.session.add(post2)
+        db.session.add(post3)
         db.session.commit()
     theadmin = Blog_User.query.all()
+
 
 # ***********************************************************************************************
 # BLOG PAGES
@@ -197,8 +202,7 @@ def contact():
             return render_template('contact.html', msg_sent=True)
         except:
             return "There was an error adding message to the database."
-    # my_contacts = db.session.query(Blog_Contact).all()
-    # print(my_contacts)
+    
     return render_template('contact.html', msg_sent=False)
 
 @app.route("/post/<int:index>")
@@ -275,9 +279,6 @@ def user_signup():
         db.session.add(new_user)
         db.session.commit()
 
-        theuser = Blog_User.query.all()
-        print(theuser)
-
         login_user(new_user)
 
         return redirect(url_for('user_dashboard'))
@@ -309,11 +310,13 @@ def user_login():
 # Super admin: ability to manage users
 
 @app.route("/user_dashboard/manage_users", methods=["GET", "POST"])
+@login_required
 def manage_users():
     all_blog_users = Blog_User.query.order_by(Blog_User.id)
     return render_template("manage_users.html", logged_in=current_user.is_authenticated, all_blog_users=all_blog_users)
 
 @app.route("/user_dashboard/manage_users/update/<int:id>", methods=["GET", "POST"])
+@login_required
 def update_users(id):
     acct_types = ["admin", "author", "user"]
     acct_blocked = ["FALSE", "TRUE"]
@@ -327,7 +330,6 @@ def update_users(id):
             flash("This username is already registered with us.")
             return render_template("manage_users_update.html", id=user_to_update.id, logged_in=current_user.is_authenticated, user_to_update=user_to_update, acct_types=acct_types, acct_blocked=acct_blocked)
         else:
-            print("in else")
             user_to_update.name = request.form.get("username_update")
             user_to_update.email = request.form.get("email_update")
             user_to_update.type = request.form.get("accttype_update")
@@ -345,6 +347,7 @@ def update_users(id):
 
 
 @app.route("/user_dashboard/manage_users/delete/<int:id>", methods=["GET", "POST"])
+@login_required
 def delete_users(id):
     user_to_delete = Blog_User.query.get_or_404(id)
     if request.method == "POST":
@@ -363,14 +366,15 @@ def delete_users(id):
         return render_template("manage_users_delete.html", logged_in=current_user.is_authenticated, user_to_delete=user_to_delete)
 
 # ***********************************************************************************************
-# Blog posts
+# Blog posts form
 class The_Posts(FlaskForm):
     theme = SelectField(u'Theme', choices=['Beach', 'City', 'Nature', 'Culture'])
-    author = StringField("Author", validators=[DataRequired()])
+    author = StringField("Author")
     date = DateTimeField('Date', default=datetime.now, format='%Y-%m-%d')
     title = StringField("Title", validators=[DataRequired()])
     intro = StringField("Intro", validators=[DataRequired()])
-    body = StringField("Body", validators=[DataRequired()], widget=TextArea())
+    # body = StringField("Body", validators=[DataRequired()], widget=TextArea())
+    body = CKEditorField("Body", validators=[DataRequired()])
     picture_v = StringField("Picture Vertical", default="Picture_v_XX.jpg", validators=[DataRequired()])
     picture_v_source = StringField("Picture Vertical", default="http://")
     picture_h = StringField("Picture Horizontal", default="Picture_h_XX.jpg", validators=[DataRequired()])
@@ -384,18 +388,23 @@ class The_Posts(FlaskForm):
 
 # Adding a new blog post
 @app.route("/user_dashboard/submit_posts", methods=["GET", "POST"])
+@login_required
 def submit_post():
     form = The_Posts()
+    # POST NOT BEING SUBMITTED ON 'SUBMIT'
     if form.validate_on_submit():
-        post = Blog_Posts(theme=form.theme.data, author=form.author.data, 
-                          date_to_post=form.date.data, title=form.title.data, intro=form.intro.data,
-                          body=form.body.data, picture_v=form.picture_v.data, picture_v_source=form.picture_v_source.data,
-                          picture_h=form.picture_h.data, picture_h_source=form.picture_h_source.data,
-                          picture_s=form.picture_s.data, picture_s_source=form.picture_s_source.data, 
-                          picture_alt=form.picture_alt.data, meta_tag=form.meta_tag.data, title_tag=form.title_tag.data)
+        print("now here")
+        author = current_user.id
+        post = Blog_Posts(theme=form.theme.data,
+                            date_to_post=form.date.data, title=form.title.data, intro=form.intro.data,
+                            body=form.body.data, picture_v=form.picture_v.data, picture_v_source=form.picture_v_source.data,
+                            picture_h=form.picture_h.data, picture_h_source=form.picture_h_source.data,
+                            picture_s=form.picture_s.data, picture_s_source=form.picture_s_source.data, 
+                            picture_alt=form.picture_alt.data, meta_tag=form.meta_tag.data, title_tag=form.title_tag.data, 
+                            author_id=author)
         # clear form:
         form.theme.data = ""
-        form.author.data = ""
+        # form.author.data = ""
         form.date.data = datetime.now
         form.title.data = ""
         form.intro.data = ""
@@ -420,25 +429,28 @@ def submit_post():
 
 # Seeing all submitted posts
 @app.route("/user_dashboard/submitted_posts")
+@login_required
 def submitted_posts():
     all_blog_posts_submitted = Blog_Posts.query.order_by(Blog_Posts.id)
     return render_template("submitted_blog_posts.html", logged_in=current_user.is_authenticated, all_blog_posts_submitted=all_blog_posts_submitted)
 
 # Previewing a post
 @app.route("/user_dashboard/preview_post/<int:id>")
+@login_required
 def preview_post(id):
     post_to_preview = Blog_Posts.query.get_or_404(id)
     return render_template("preview_post.html", logged_in=current_user.is_authenticated, post_to_preview=post_to_preview)
 
 # Editing a post
 @app.route("/user_dashboard/editting_post/<int:id>", methods=["GET", "POST"])
+@login_required
 def editing_posts(id):
     post_to_edit = Blog_Posts.query.get_or_404(id)
     form = The_Posts()
     # changing the post
     if form.validate_on_submit():
         post_to_edit.theme = form.theme.data
-        post_to_edit.author = form.author.data
+        # post_to_edit.author = form.author.data #author not available anymore, use author_id
         post_to_edit.date_to_post = form.date.data
         post_to_edit.title = form.title.data
         post_to_edit.intro = form.intro.data
@@ -459,7 +471,7 @@ def editing_posts(id):
         return redirect(url_for("submitted_posts", logged_in=current_user.is_authenticated))
     # filling out the form with saved post data
     form.theme.data = post_to_edit.theme
-    form.author.data = post_to_edit.author
+    form.author.data = post_to_edit.author.name
     form.date.data = post_to_edit.date_to_post
     form.title.data = post_to_edit.title
     form.intro.data = post_to_edit.intro
@@ -477,6 +489,7 @@ def editing_posts(id):
 
 # Deleting a post
 @app.route("/user_dashboard/deleting_post/<int:id>", methods=["GET", "POST"])
+@login_required
 def deleting_posts(id):
     post_to_delete = Blog_Posts.query.get_or_404(id)
     if request.method == "POST":
@@ -491,9 +504,54 @@ def deleting_posts(id):
     else:
         return render_template("manage_posts_delete.html", logged_in=current_user.is_authenticated, post_to_delete=post_to_delete)
 
+# ***********************************************************************************************
+# Manage Own Account
+
+@app.route("/user_dashboard/manage_account")
+@login_required
+def manage_acct():
+    return render_template("manage_acct.html", logged_in=current_user.is_authenticated)
 
 
+@app.route("/user_dashboard/manage_acct/update/<int:id>", methods=["GET", "POST"])
+@login_required
+def update_own_acct_info(id):
+    user_at_hand = Blog_User.query.get_or_404(id)
 
+    if request.method == "POST":
+        user_at_hand.name = request.form.get("username_update")
+        user_at_hand.email = request.form.get("email_update")
+        user_at_hand.about = request.form.get("about_update")
+        try:
+            db.session.commit()
+            flash("Acct info updated successfully!")
+            # no time for flash, change way of displaying success
+            return redirect(url_for('manage_acct'))
+        except:
+            flash("Oops, error, try again.")
+            return redirect(url_for('manage_acct'))
+    else:
+        return render_template("manage_acct_update.html", logged_in=current_user.is_authenticated)
+
+
+@app.route("/user_dashboard/manage_acct/delete/<int:id>", methods=["GET", "POST"])
+@login_required
+def delete_own_acct(id):
+    user_at_hand = Blog_User.query.get_or_404(id)
+    if request.method == "POST":
+        if id == 1:
+            flash("Authorization error: this user cannot be deleted")
+        else:
+            try:
+                db.session.delete(user_at_hand)
+                db.session.commit()
+                flash("Your acct was deleted successfully.")
+                return redirect(url_for("home"))
+            except:
+                flash("There was a problem deleting your account.")
+                return redirect(url_for('manage_acct'))
+    else:
+        return render_template("manage_acct_delete.html", logged_in=current_user.is_authenticated)
 # ***********************************************************************************************
 # 404 and 500 Errors
 @app.errorhandler(404)
