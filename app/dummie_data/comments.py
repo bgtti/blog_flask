f6d7c125ac1a9b25880a75721951615a749c0916
Bgtti <97987480+bgtti@users.noreply.github.com>
2023-04-02 23:19:41 +0200

    Add files via upload

---
+++ app/dummie_data/comments.py
@@ -0,0 +1,28 @@
+# Comments and replies
+
+comment_data = [
+    {
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lectus sed nulla scelerisque tempus."
+    },
+    {
+        "text": "Ed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+    },
+    {
+        "text": "Sed pharetra lectus sed nulla scelerisque tempus."
+    },
+    {
+        "text": "Thank you for such a great post.",
+    },
+    {
+        "text": "I have a question about this.",
+    },
+    {
+        "text": "Let me know, happy to answer.",
+    },
+    {
+        "text": "This sounds like an awesome trip.",
+    },
+    {
+        "text": "Yes, I like it too.",
+    }
+]
